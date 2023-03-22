654a00998a6e36d440c888c615c2408e94e5d233
Logan Cole <emtlogancole@gmail.com>
2023-03-22 17:30:12 -0500

    test quote index

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < ApplicationController
   def index
-  
+    @quotes = Quote.all
   end
 
   def show
--- spec/features/people/show_spec.rb
+++ spec/features/people/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Person do
   before(:each) do
-    @person = Person.create(name: "Logan", deceased: false, birthday: 19960401)
+    @person = Person.create(name: "Logan", deceased: false, birthday: 2005-10-10)
   end
   it "displays the persons name" do
     visit "/people/#{@person.id}"
